ba005ba2125d15597385708b02b37ca941e9c666
romainrhd <romain.richard.19@gmail.com>
2022-03-16 18:09:19 +0100

    Add graph and begin configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Pokemon } from './entities/pokemon.entity';
 import { Appearance } from './entities/appearance.entity';
 import { PokemonsService } from './pokemons/pokemons.service';
 import { PokemonsController } from './pokemons/pokemons.controller';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+    }),
     TypeOrmModule.forRoot(),
     TypeOrmModule.forFeature([Appearance, Pokemon]),
   ],
